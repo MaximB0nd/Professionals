20932f839ee0a6987d055c88c5a9bf73a56340d6
MaximB0nd <sersim11@yandex.ru>
2026-01-19 09:43:06 +0300

    feat: add titanic clering

--- titanic_clearing.py
+++ titanic_clearing.py
@@ -17,10 +17,22 @@ sns.boxplot(df['Age'].dropna())
 
 # plt.show()
 
-df_median = df['Age'].median()
-df["Age"] = df["Age"].fillna(df_median)
+df_age_median = df['Age'].median()
+df["Age"] = df["Age"].fillna(df_age_median)
 # print(df["Age"].isnull().sum())
 
 df = df.drop(columns=['Cabin'])
+
+# print(df['Embarked'].value_counts())
+df_embarked_median = df['Embarked'].mode()[0]
+# print(df_embarked_median)
+
+df['Embarked'] = df['Embarked'].fillna(df_embarked_median)
+# print(df.isnull().sum())
+
+# print(df["Sex"].value_counts())
+df['Sex'] = df['Sex'].map({'male': 1, 'female': 0})
+# df['Sex'] = df['Sex'].apply(lambda x: 0 if x == 'male' else 1)
+
 
 
